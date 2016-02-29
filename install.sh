feb82da240c23cbffbc6a652820d41e21934604f
Colin Haley <admin@colinhaley.net>
2016-02-28 21:41:21 -0500

    add pip installer

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[$EUID -ne 0]]; then
 	echo "This script must be run as root"
 	exit 1
@@ -14,7 +16,13 @@ if check_command python ; then
 	echo "Python already installed"
 else
 	apt-get install python
+
+# Pip installer?
+	wget https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py
+	python /tmp/get-pip.py
 fi
+
+
 
 # Git Requirement
 if check_command git ; then
@@ -28,10 +36,13 @@ if check_command ansible ; then
 	echo "Ansible already installed."
 	exit 1
 else
-cd /
-git clone git://github.com/ansible/ansible.git --recursive
-cd ansisble/playbooks
-source .hacking/env-setup
+	cd /
+	git clone git://github.com/ansible/ansible.git --recursive
+	cd ansisble/playbooks
+	source ./hacking/env-setup
+
+		
+
 fi
 
 
