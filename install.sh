e7fbfca2232c634a094a2436a5c82caf5d6a874e
Colin Haley <admin@colinhaley.net>
2016-02-28 21:31:43 -0500

    ansible clone working flawlessly

--- install.sh
+++ install.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
 if [[$EUID -ne 0]]; then
-echo "This script must be run as root"
-exit 1
-
+	echo "This script must be run as root"
+	exit 1
 else
 
 check_command() {
-type "$1" &> /dev/null ;
+	type "$1" &> /dev/null ;
 }
 
+# Python Requirement
 if check_command python ; then
-apt-cyg install python
+	echo "Python already installed"
+else
+	apt-get install python
 fi
+
+# Git Requirement
+if check_command git ; then
+	echo "Git already installed"
+else
+	apt-get install git
+fi
+
+#create ansible dir
+if check_command ansible ; then
+	echo "Ansible already installed."
+	exit 1
+else
+cd /
+git clone git://github.com/ansible/ansible.git --recursive
+cd ansisble/playbooks
+source .hacking/env-setup
+fi
+
+
 
 
 fi
