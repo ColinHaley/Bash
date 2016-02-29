48f5450f82f64e885472f94dee33490c5b155158
Colin Haley <admin@colinhaley.net>
2016-02-28 21:21:33 -0500

    Add starter

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+if [[$EUID -ne 0]]; then
+echo "This script must be run as root"
+exit 1
+
+else
+
 check_command() {
 type "$1" &> /dev/null ;
 }
 
-if check_command  ; then
+if check_command python ; then
+apt-cyg install python
+fi
+
 
 fi
